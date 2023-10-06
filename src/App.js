e1a050864b0dbb5c621487bbdb89c77c33154cde
hugriveros <hugoriveros14@gmail.com>
2023-10-06 11:51:57 -0300

    asasd
    actualizaciones

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import FacebookLogin from 'react-facebook-login';
 
 function App() {
+
+useEffect({},[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FacebookLogin
+    appId="1680733605754642"
+    autoLoad={true}
+    fields="name,email,picture"
+    onClick={componentClicked}
+    callback={responseFacebook} />,
     </div>
   );
 }
